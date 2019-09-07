b8909c0eddfff5f46b807665e39238958145ecf9
iobarkalov <barkalov@greenmoney.ru>
2019-09-07 16:56:41 +0300

    Создал контроллер для авторизации пользователей

---
+++ Cost.Api/Cost.Api/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+
+namespace Cost.Api.Controllers
+{
+    /// <summary>
+    /// Контроллер для авторизации/регистрации пользователей
+    /// </summary>
+    [Route("[controller]")]
+    public class AccountController : ApiController
+    {
+        /// <summary>
+        /// Авторизация в приложении
+        /// </summary>
+        [HttpPost]
+        public IHttpActionResult Login()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Регистрация в приложении
+        /// </summary>
+        [HttpPost]
+        public IHttpActionResult Registration()
+        {
+            return Ok();
+        }
+    }
+}
